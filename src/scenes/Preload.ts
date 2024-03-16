1930f2b76dbdc151d1c8530b391162c755461312
Andreas Schrottenbaum <andreas.schrottenbaum@3q.video>
2024-03-17 00:49:26 +0100

    removed second loop

--- src/scenes/Preload.ts
+++ src/scenes/Preload.ts
@@ -12,29 +12,19 @@ export default class Preload extends Phaser.Scene {
     this.load.atlas('player', 'assets/gfx/character/character.png', 'assets/gfx/character/character.json')
   }
 
-  create() { 
-    const actions = ['walk', 'carry', 'attack']
+  create() {
+    const actions: [animationName: string, frameCount: number][] = [['walk', 4], ['carry', 4], ['attack', 4], ['grab', 3]]
     const directions = ['up', 'down', 'left', 'right']
 
     // create animations for walking, carrying, and attacking in all directions
-    actions.forEach((action) => {
+    actions.forEach(([animationName, frameCount]) => {
       directions.forEach((dir) => {
         this.anims.create({
-          key: `player-${action}-${dir}`,
-          frames: this.anims.generateFrameNames('player', { start: 1, end: 4, prefix: `${action}-${dir}-`, suffix: '.png' }),
+          key: `player-${animationName}-${dir}`,
+          frames: this.anims.generateFrameNames('player', { start: 1, end: frameCount, prefix: `${animationName}-${dir}-`, suffix: '.png' }),
           repeat: -1,
           frameRate: 8
         })
-      })
-    })
-    
-    // create animations for grabbing in all directions
-    directions.forEach((dir) => {
-      this.anims.create({
-        key: `player-grab-${dir}`,
-        frames: this.anims.generateFrameNames('player', { start: 1, end: 3, prefix: `grab-${dir}-`, suffix: '.png' }),
-        repeat: -1,
-        frameRate: 8
       })
     })
 
